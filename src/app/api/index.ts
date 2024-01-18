bb4162e04dd6dd241e284e2d74b53651605a5cfc
Dilshad Matkulov <dilshadmatkulov@gmail.com>
2024-01-18 13:28:50 +0600

    Restructured folder

--- src/app/api/index.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-
-const Caesar = require('caesar-salad').Caesar;
-
-const app = express();
-const port = 8000;
-
-app.use(express.json())
--- src/types.d.ts
+++
@@ -1,4 +0,0 @@
-export interface Message {
-  password: string;
-  message: string;
-}
